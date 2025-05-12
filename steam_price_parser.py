aa91181c2fd94a64b4aad6d065ec0b1b6cc19f5a
xxqqwua <xxqqwua@gmail.com>
2025-05-12 21:12:48 +0300

    add: regions

--- steam_price_parser.py
+++ steam_price_parser.py
@@ -1,6 +1,34 @@
-
+import asyncio
 
 
 class SteamPriceParser:
     def __init__(self):
-        pass
+        self.regions = {
+            'us': 'USA',
+            'ca': 'Canada',
+            'gb': 'Great Britain',
+            'eu': 'Europe',  # European Union
+            'ru': 'Russia',
+            'ua': 'Ukraine',
+            'kz': 'Kazakhstan',
+            # 'tr': 'Turkey',
+            'ar': 'Argentina',
+            'br': 'Brazil',
+            'mx': 'Mexico',
+            'in': 'India',
+            'cn': 'China',
+            'jp': 'Japan',
+            'au': 'Australia',
+            'nz': 'New Zealand',
+            'za': 'South Africa',
+            'ch': 'Switzerland',
+            'no': 'Norway',
+            'se': 'Sweden',
+            'dk': 'Denmark',
+            'pl': 'Poland',
+            'cz': 'Czech Republic',
+            'hu': 'Hungary',
+            'sg': 'Singapore',
+            'hk': 'Hong Kong',
+            'kr': 'Korea',
+        }
