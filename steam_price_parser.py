f22d90365e070fe1f7e6a6ff55507654e7100de3
xxqqwua <xxqqwua@gmail.com>
2025-05-12 22:34:12 +0300

    add: added more regions; add: added support for different input data types for specifying regions; add: added recognizing game availability in a region; refactor and optimization: renamed country names to their currency

--- steam_price_parser.py
+++ steam_price_parser.py
@@ -1,34 +1,98 @@
-import asyncio
+import aiohttp
 
 
 class SteamPriceParser:
     def __init__(self):
         self.regions = {
-            'us': 'USA',
-            'ca': 'Canada',
-            'gb': 'Great Britain',
-            'eu': 'Europe',  # European Union
-            'ru': 'Russia',
-            'ua': 'Ukraine',
-            'kz': 'Kazakhstan',
+            'us': 'U.S. Dollar',
+            'ca': 'Canadian Dollar',
+            'uk': 'British Pound',
+            'eu': 'Euro',
+            'ru': 'Russian Ruble',
+            'ua': 'Ukrainian Hryvnia',
+            'kz': 'Kazakhstani Tenge',
             # 'tr': 'Turkey',
-            'ar': 'Argentina',
-            'br': 'Brazil',
-            'mx': 'Mexico',
-            'in': 'India',
-            'cn': 'China',
-            'jp': 'Japan',
-            'au': 'Australia',
-            'nz': 'New Zealand',
-            'za': 'South Africa',
-            'ch': 'Switzerland',
-            'no': 'Norway',
-            'se': 'Sweden',
-            'dk': 'Denmark',
-            'pl': 'Poland',
-            'cz': 'Czech Republic',
-            'hu': 'Hungary',
-            'sg': 'Singapore',
-            'hk': 'Hong Kong',
-            'kr': 'Korea',
+            # 'ar': 'Argentina',
+            'br': 'Brazilian Real',
+            'mx': 'Mexican Peso',
+            'in': 'Indian Rupee',
+            'cn': 'Chinese Yuan',
+            'jp': 'Japanese Yen',
+            'au': 'Australian Dollar',
+            'nz': 'New Zealand Dollar',
+            'za': 'South African Rand',
+            'ch': 'Swiss Franc',
+            'no': 'Norwegian Krone',
+            'pl': 'Polish Zloty',
+            'sg': 'Singapore Dollar',
+            'hk': 'Hong Kong Dollar',
+            'kr': 'South Korean Won',
+            'co': 'Colombian Peso',
+            'vn': 'Vietnamese Dong',
+            'id': 'Indonesian Rupiah',
+            'cl': 'Chilean Peso',
+            'pk': 'South Asia - USD',
+            'ph': 'Philippine Peso',
+            'uy': 'Uruguayan Peso',
+            'pe': 'Peruvian Sol',
+            'my': 'Malaysian Ringgit',
+            'az': 'CIS - U.S. Dollar',
+            'th': 'Thai Baht',
+            'tw': 'Taiwan Dollar',
+            'sa': 'Saudi Riyal',
+            'qa': 'Qatari Riyal',
+            'kw': 'Kuwaiti Dinar',
+            'cr': 'Costa Rican Colon',
+            'ae': 'U.A.E. Dirham',
+            'il': 'Israeli New Shekel'
         }
+
+        self.prices = {}
+
+    async def check_price(self, app_id, regions='all'):
+        if regions == 'all' or regions == '':
+            iterator = self.regions.items()
+        else:
+            if isinstance(regions, str):
+                # If regions is a string, split it by comma and remove spaces around each code.
+                # The split(‘,’) method splits the string into substrings, and strip() removes extra spaces.
+                region_codes = [code.strip() for code in regions.split(',')]
+            elif isinstance(regions, list):
+                region_codes = regions  # If regions is a list, just use it as is.
+            elif isinstance(regions, dict):
+                region_codes = regions.keys()  # If regions is a dictionary, iterate over its keys.
+            else:
+                raise ValueError(
+                    f"Unsupported region type: {type(regions).__name__}")  # If regions is not a string or a list, throw an error.
+
+            # Create an iterator that will return pairs (country code, name).
+            # self.regions.get() safely gets the country name by code, returning ‘Unknown (code)’ if no code is found.
+            iterator = ((cc, self.regions.get(cc, f"Unknown ({cc})")) for cc in region_codes)
+
+        async with aiohttp.ClientSession() as session:
+            for cc, region in iterator:
+                url = f"https://store.steampowered.com/api/appdetails?appids={app_id}&cc={cc}&l=en"
+
+                try:
+                    async with session.get(url) as r:
+                        raw = await r.json()
+                        is_aviable = raw[str(app_id)]['success']
+
+                        if is_aviable is False:
+                            self.prices[region] = 'The game is not available in this region'
+                            continue
+
+                        price = raw[str(app_id)]['data'].get('price_overview')
+
+                        if price:
+                            if 'Unknown' in region:
+                                raise ValueError(f"Unsupported region type: {region}")
+
+                            current_price = price['final_formatted']
+                            self.prices[region] = current_price
+                        else:
+                            self.prices[region] = "Unable to find a price"
+                            continue
+
+                except Exception as e:
+                    raise RuntimeError(f"Error during region processing {cc}: {e}")
